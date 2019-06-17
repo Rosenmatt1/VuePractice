40064479f12b3f8c7eca3b7808b644bb75cb2033
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-06-17 17:45:52 -0600

    Styled the button and put as global css

--- vue-to-do-list/src/App.vue
+++ vue-to-do-list/src/App.vue
@@ -57,4 +57,17 @@ export default {
     font-family: 'Avenir', Helvetica, sans-serif;
     line-height: 1.4;
   }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background: #666;
+  }
 </style>
