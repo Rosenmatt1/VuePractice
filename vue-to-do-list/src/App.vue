36a23ca8ba53f598ca0775656bedd71e80580e1e
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-06-20 18:26:22 -0600

    created the new todo with the response from the post request becasue it includes the id

--- vue-to-do-list/src/App.vue
+++ vue-to-do-list/src/App.vue
@@ -45,7 +45,13 @@ export default {
       this.todos = this.todos.filter(todo => todo.id !== id)
     },
     addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo]
+      const { title, completed } = newTodo
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed
+      })
+        .then(res => this.todos = [...this.todos, res.data])
+        .catch(err =>console.log(err))
     }
   },
   created() {
