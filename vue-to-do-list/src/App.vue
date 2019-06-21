bacd36466c25309b86c53634112cbe9cde24b90d
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-06-20 18:36:52 -0600

    Put the state update in the response from the delete request

--- vue-to-do-list/src/App.vue
+++ vue-to-do-list/src/App.vue
@@ -42,7 +42,9 @@ export default {
   },
   methods: {
     deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id)
+      axios.delete(`http://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res => this.todos = this.todos.filter(todo => todo.id !== id))
+      .catch(err =>console.log(err))
     },
     addTodo(newTodo) {
       const { title, completed } = newTodo
