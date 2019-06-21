ebfd049196bc9a56f051aa52b7465c3f23a575fe
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-06-21 15:19:53 -0600

    Pasted in the App.vue into the Home.vue and updated the paths

--- vue-to-do-list/src/views/Home.vue
+++ vue-to-do-list/src/views/Home.vue
@@ -1,18 +1,94 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Header />
+    <AddToDo v-on:add-todo="addTodo"/>
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '../components/Layout/Header'
+import Todos from '../components/Todos'
+import AddToDo from '../components/AddToDo'
+import axios from "axios"
+
+ // {
+        //   id: 1,
+        //   title: "Todo One",
+        //   completed: false
+        // },
+        // {
+        //   id: 2,
+        //   title: "Todo Two",
+        //   completed: true
+        // },
+        // {
+        //   id: 3,
+        //   title: "Todo Three",
+        //   completed: false
+        // }
 
 export default {
-  name: 'home',
+  name: 'Home',
   components: {
-    HelloWorld
-  }
+    Todos,
+    Header,
+    AddToDo
+  },
+  data() {
+    return {
+      todos: []
+    }
+  },
+  methods: {
+    deleteTodo(id) {
+      axios.delete(`http://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res => this.todos = this.todos.filter(todo => todo.id !== id))
+      .catch(err =>console.log(err))
+    },
+    addTodo(newTodo) {
+      const { title, completed } = newTodo
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed
+      })
+        .then(res => this.todos = [...this.todos, res.data])
+        .catch(err =>console.log(err))
+    }
+  },
+  created() {
+      axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+        .then(res => this.todos = res.data)
+        .catch(err => console.log(err))
+      // fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      //   .then(response => response.json())
+      //   .then(json => this.todos = json.data)
+      //   console.log(this.todos)
+    }
 }
 </script>
+
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: 'Avenir', Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background: #666;
+  }
+</style>
