cafa30bbd712a6ef8dce28065fd8266b32a86130
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-06-21 16:10:05 -0600

    Passed in store component to header and router.js

--- vue-to-do-list/src/views/Store.vue
+++ vue-to-do-list/src/views/Store.vue
@@ -1,6 +1,38 @@
 <template>
-  <div class="about">
+  <div class="store">
     <h1>Store</h1>
     <p>This is where to shop</p>
   </div>
 </template>
+
+<script>
+
+export default {
+  name: 'Store',
+}
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: 'Avenir', Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background: #666;
+  }
+</style>
