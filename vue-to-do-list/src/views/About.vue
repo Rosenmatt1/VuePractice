6ea67e3da7e48eea55be5fa33bd1a6634211830a
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-06-23 22:57:07 -0600

    Passed attribute prop to About page

--- vue-to-do-list/src/views/About.vue
+++ vue-to-do-list/src/views/About.vue
@@ -2,5 +2,14 @@
   <div class="about">
     <h1>About</h1>
     <p>This is the about page.</p>
+    <p>{{attribute}}</p>
   </div>
 </template>
+
+
+<script>
+export default {
+  props: ["attribute"]
+}
+</script>
+
--- vue-to-do-list/src/views/Home.vue
+++ vue-to-do-list/src/views/Home.vue
@@ -31,7 +31,7 @@ export default {
   data() {
     return {
       todos: [],
-      attribute: true
+      attribute: "tester"
     }
   },
   methods: {
